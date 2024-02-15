d8a8676ae73a37a59283c9b8135250c9bd4ff424
wamuni <boshi.pan@outlook.com>
2024-02-15 00:48:04 +0000

    difference between struct and map

--- maps/main.go
+++ maps/main.go
@@ -9,6 +9,7 @@ func main() {
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
+		"white": "#ffffff",
 	}
 	fmt.Println(colors)
 	// Syntax 2
@@ -24,4 +25,18 @@ func main() {
 	fmt.Println(colors3)
 
 	//TODO: Is there any difference between different syntax
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for ", color, " is ", hex)
+	}
+	/**
+	* Difference between Map and Struct
+	* 1. in Map, all keys and values must have the same data type, while in struct, it can be different
+	* 2. Struct is value type, while the Map is reference type
+	* 3. Keys supports indexing in map, not in struct
+	* 4. struct needs to know all the types during the compiling time
+	 */
 }
