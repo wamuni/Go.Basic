79c2675732e5102521ccefc4ec16b8ae0e426909
wamuni <boshi.pan@outlook.com>
2024-02-15 02:16:04 +0000

    Understand interface Reader and how to use it to get resposne body

--- https/main.go
+++ https/main.go
@@ -12,5 +12,14 @@ func main() {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+	fmt.Println(resp.Status)
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Proto)
+	response_body := make([]byte, 9999)
+	n, err := resp.Body.Read(response_body)
+	if err != nil {
+		fmt.Print("Error: ", err)
+	}
+	fmt.Println("number returns: ", n)
+	fmt.Println(string(response_body))
 }
